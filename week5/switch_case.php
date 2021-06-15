dad701fa506d3ae99028ff9626ad090395fd2117
Watchara Ketkaew <wk996110@gmailcom>
2021-06-15 11:19:21 +0700

    Update Week 5

---
+++ week5/switch_case.php
@@ -0,0 +1,32 @@
+<?php  
+    $month = 2;
+
+    switch($month){
+        case 1 : echo "มกราคม <br>";
+        break;
+        case 2 : echo "กุมภาพันธ์<br>";
+        break;
+        case 3 : echo "มีนาคม<br>";
+        break;
+        case 4 : echo "เมษายน<br>";
+        break;
+        case 5 : echo "พฤษภาคม<br>";
+        break;
+        case 6 : echo "มิถุนายน<br>";
+        break;
+        case 7 : echo "กรกฎาคม<br>";
+        break;
+        case 8 : echo "สิงหาคม<br>";
+        break;
+        case 9 : echo "กันยายน<br>";
+        break;
+        case 10 : echo "ตุลาคม<br>";
+        break;
+        case 11 : echo "พฤศจิกายน<br>";
+        break;
+        case 12 : echo "ธันวาคม<br>";
+        break;
+        default : echo "ไม่พบเดือน<br>";
+
+    }
+?>
